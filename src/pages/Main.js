22a2fc179641fede0678ed9e5136929b13988df5
wkqkel <89131236+wkqkel@users.noreply.github.com>
2022-02-12 10:25:48 +0900

    헤더높이지정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,17 +1,38 @@
 import React from "react"
 import { Button, Text, Grid, Input } from "../elements"
 import styled from "styled-components"
-
+import Post from "../components/Post"
 const Main = (props) => {
   return (
-    <Grid is_flex>
-      <Button padding="20px" bg="#fff">
-        fhdjfhdjk
-      </Button>
-      <Input></Input>
-      <Text>gkgk</Text>
-    </Grid>
+    <>
+      <Container>
+        <Post_container>
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
+        </Post_container>
+      </Container>
+    </>
   )
 }
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-item: center;
+  margin: 0;
+  padding: 0% 8%;
+`
+const Post_container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    justify-content: center;
+  }
+`
 export default Main
