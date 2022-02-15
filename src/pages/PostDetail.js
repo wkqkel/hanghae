d974e59cbff542815f4372c5327a01b940e8dbb9
wkqkel <89131236+wkqkel@users.noreply.github.com>
2022-02-15 15:28:49 +0900

    참여하기 기능구현중 + 디테일페이지 새로고침오류해결

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -8,23 +8,44 @@ import styled from "styled-components"
 import { useSelector, useDispatch } from "react-redux"
 import { actionCreators as commentsActions } from "../redux/modules/comments"
 
+import { actionCreators as postActions } from "../redux/modules/post"
+
 const PostDetail = (props) => {
   const dispatch = useDispatch()
   const [comment, setComment] = useState("")
 
   const id = props.match.params.id
-  console.log(id)
 
-  let userId = localStorage.getItem("userId")
-  console.log(userId)
+  let [isJoin, setIsJoin] = React.useState(false)
 
-  const post_list = useSelector((store) => store.post.list)
-  const post = post_list.find((p) => p.postId === id)
-  console.log(post)
-
+  const loginUserName = localStorage.getItem("loginUserName")
   React.useEffect(() => {
     dispatch(commentsActions.getCommentFB(id))
+    // 페이지 새로고침 시 포스트 1개만 새로 요청하기
+    // 발생한 이슈 useEffect가 비동기로 처리돼서 처음에 []을 아래에서 읽으려고 해서 에러 발생_ 해결방법 3가지
+    // 1. 리턴 아래부분을 컴포넌트로 빼서 props로 넘겨주는 방법
+    // 2. useSelector을 사용하지않고, 클릭하는 버튼에서 링크투로 부모 컴포넌트에서 리스트를 넘겨받는 방법
+    // 3. 이번에는 찾아본 결과_ 아래 리턴 부분에 해당 데이터가 쓰이는 곳에 post&& 같은 조건식을 달아주어서 빈값이면 아예 접근 못하게 해서 해결.
+    if (!post) {
+      dispatch(postActions.getOnePostDB(id))
+    }
+
+    if (post && post.curMembers.includes(loginUserName)) {
+      setIsJoin(true)
+    }
   }, [])
+  const post_list = useSelector((store) => store.post.list)
+  const post = post_list.find((p) => p.postId === id)
+
+  const clickJoin = () => {
+    // let loginUser = { userName: loginUserName }
+    // setIsJoin(!isJoin)
+    // if (isJoin) {
+    //   dispatch(postActions.deleteJoinDB(id, loginUser))
+    // } else {
+    //   dispatch(postActions.addJoinDB(id, loginUser))
+    // }
+  }
 
   const onChange = (e) => {
     setComment(e.target.value)
@@ -37,41 +58,47 @@ const PostDetail = (props) => {
     dispatch(commentsActions.addCommentFB(id, comments))
     setComment("")
   }
-
   return (
     <Container>
       <Grid margin="30px 0px">
         <Grid is_flex>
-          <Category>{post.category}</Category>
-          <Title>{post.title}</Title>
+          <Category>{post && post.category}</Category>
+          <Title>{post && post.title}</Title>
         </Grid>
         <Grid is_flex padding="10px">
-          <Text bold>{post.userName}</Text>
-          <Text bold>{post.createDate}</Text>
+          <Text bold>{post && post.userName}</Text>
+          <Text bold>{post && post.createDate}</Text>
         </Grid>
-        <Contents>{post.contents}</Contents>
+        <Contents>{post && post.contents}</Contents>
         <Grid is_flex>
           <Grid width="auto" padding="10px">
             <Grid padding="10px" width="110px">
-              <Text>{post.deadLine}</Text>
+              <Text>{post && post.deadLine}</Text>
             </Grid>
           </Grid>
           <Grid is_flex margin="0px 5px">
             <Text>
-              {post.curMembers.length} / {post.maxMembers}
+              {post && post.curMembers.length} / {post && post.maxMembers}
             </Text>
-            <Button disable width="100px">
-              참여하기
+
+            <Button width="100px" _onClick={clickJoin}>
+              {post &&
+              post.curMembers.length === post.maxMembers &&
+              !post.curMembers.includes(loginUserName)
+                ? "마감완료"
+                : isJoin
+                ? "참여취소"
+                : "참여하기"}
             </Button>
           </Grid>
         </Grid>
         <Grid is_flex width="auto">
-          {/* <Button disable width="60px" margin="0px 10px">
-              수정
-            </Button>
-            <Button disable width="60px">
-              삭제
-            </Button> */}
+          <Button disable width="60px" margin="0px 10px">
+            수정
+          </Button>
+          <Button disable width="60px">
+            삭제
+          </Button>
         </Grid>
       </Grid>
       <Grid bg="#E8F3F1" borderRadius>
