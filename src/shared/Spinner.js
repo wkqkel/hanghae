ddb185c01a86c465dcb9fd76597bc00e19a553c2
Bong <borobong230@gmail.com>
2022-02-17 19:23:59 +0900

    momin final!
    momin final!

--- src/shared/Spinner.js
+++ src/shared/Spinner.js
@@ -4,7 +4,7 @@ import styled from "styled-components"
 const Spinner = (props) => {
   return (
     <Outter>
-      <img src="https://firebasestorage.googleapis.com/v0/b/megazine-11a01.appspot.com/o/images%2Fmomin1.png?alt=media&token=474370a1-1e4b-4883-83e0-dda39b708bd3" />
+      <Image src="https://firebasestorage.googleapis.com/v0/b/megazine-11a01.appspot.com/o/images%2Fmomin1.png?alt=media&token=474370a1-1e4b-4883-83e0-dda39b708bd3" />
     </Outter>
   )
 }
@@ -20,6 +20,13 @@ const Outter = styled.div`
   justify-content: center;
   background-color: aliceblue;
   z-index: 100;
+  }
+`
+const Image = styled.img`
+  @media only screen and (max-width: 768px) {
+    width: 80%;
+    justify-content: center;
+  }
 `
 
 export default Spinner
