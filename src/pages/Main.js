e479407180e6b932bdfb21bb2bf8b74e8d0bc927
wkqkel <89131236+wkqkel@users.noreply.github.com>
2022-03-04 16:20:27 +0900

    기본셋팅-1

---
+++ src/pages/Main.js
@@ -0,0 +1,28 @@
+import React from "react";
+// import { Button, Grid, Input, Text } from "../elements";
+import styled from "styled-components";
+// import { FaHeart } from "react-icons/fa";
+// import Post from "../components/Post";
+// import { useDispatch, useSelector } from "react-redux";
+// import { actionCreators as PostActions } from "../redux/modules/post";
+// import { actionCreators as CommonActions } from "../redux/modules/common";
+
+const Main = () => {
+  //   const dispatch = useDispatch();
+  //   const postList = useSelector((state) => state.post.list);
+  //   React.useEffect(() => {
+  //     dispatch(PostActions.getPostDB());
+  //   }, []);
+
+  return (
+    <>
+      <Container>확인</Container>
+    </>
+  );
+};
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export default Main;
